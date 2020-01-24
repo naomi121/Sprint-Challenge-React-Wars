27edd3e22714a10f723ef98d3884524eea3075dc
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-24 11:18:19 -0500

    added useEffect Axios.get in line 28 App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import styled from "styled-components";
 import Axios from "axios";
 
+import CharacterCard from "./components/Character.js";
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,6 +21,24 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    Axios.get('https://swapi.co/api/people/')
+      .then(response => {
+        //console.log(response);
+        setCharacters(response.data.results);
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+
+
+
+
+
+
 
   return (
     <div className="App">
